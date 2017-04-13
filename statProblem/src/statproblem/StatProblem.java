1b6aff9b3e40addd87c9454b055bd97e827154a9
Kari <valyamercury@gmail.com>
2017-04-14 01:43:15 +1000

    Add files via upload

---
+++ statProblem/src/statproblem/StatProblem.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statproblem;
+
+/**
+ *
+ * @author Карина
+ */
+public class StatProblem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double a1 = 12398.4;
+        double a2, a3, a4, a5, a6, a7, a8;
+        double range = (66490.7-a1)/7;
+        double ran = 7728;
+        
+        a2 = a1+ran;
+        a3 = a2+ran;
+        a4 = a3+ran;
+        a5 = a4+ran;
+        a6 = a5+ran;
+        a7 = a6+ran;
+        a8 = a7+ran;
+        
+        
+        System.out.println(range);
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        
+        double bb = (62630.4-25945.7)*(62630.4-25945.7);
+        System.out.println(bb);
+        
+        System.out.println("***********************************************");
+        a1 = 1.8;
+        
+        ran = 1421;
+        
+        a2 = a1+ran;
+        a3 = a2+ran;
+        a4 = a3+ran;
+        a5 = a4+ran;
+        a6 = a5+ran;
+        a7 = a6+ran;
+        a8 = a7+ran;
+        
+        
+        
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        
+    }
+    
+}
